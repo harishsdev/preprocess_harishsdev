f7a062d7b4345bd22f6935c29a704f777c7a8ac3
harish shankam <harishsdev@gmail.com>
2020-11-15 21:48:40 +0530

    fixed errors version 3

--- preprocess_harishsdev/__init__.py
+++ preprocess_harishsdev/__init__.py
@@ -24,16 +24,16 @@ def get_mentions_counts(x):
 def get_digit_counts(x):
     return utils._get_digit_counts(x)
 
- def get_uppercase_counts(x):
+def get_uppercase_counts(x):
     return utils._get_uppercase_counts(x)
 
- def get_cont_to_exp(x):
+def get_cont_to_exp(x):
     return utils._get_cont_to_exp(x)
 
 def get_emails(x):
     return utils._get_emails(x)
 
- def remove_emails(x):
+def remove_emails(x):
     return utils._remove_emails(x)
 
 def get_urls(x):
