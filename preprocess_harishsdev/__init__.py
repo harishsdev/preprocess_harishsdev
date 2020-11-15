0330ae5d9022945ae65305b9d9c4c8c6a37c33eb
harish shankam <harishsdev@gmail.com>
2020-11-15 21:29:39 +0530

    fixed errors version 2

--- preprocess_harishsdev/__init__.py
+++ preprocess_harishsdev/__init__.py
@@ -1,6 +1,6 @@
 from preprocess_harishsdev import utils
 
-__version__='0.0.1'
+__version__='0.0.2'
 
 
 def get_word_counts(x):
@@ -16,43 +16,43 @@ def get_char_counts(x):
     return utils._get_stopwords_counts(x)
 
 def get_hashtag_counts(x):
-	return utils._get_hashtag_counts(x):
+	return utils._get_hashtag_counts(x)
 
 def get_mentions_counts(x):
-	return utils._get_mentions_counts(x):
+	return utils._get_mentions_counts(x)
 
 def get_digit_counts(x):
-   return utils._get_digit_counts(x):
+   return utils._get_digit_counts(x)
 
  def get_uppercase_counts(x):
  	return utils._get_uppercase_counts(x)
 
  def get_cont_to_exp(x):
-    return utils._get_cont_to_exp(x):
+    return utils._get_cont_to_exp(x)
 
 def get_emails(x):
     return utils._get_emails(x)
 
  def remove_emails(x):
- 	return utils._remove_emails(x):
+ 	return utils._remove_emails(x)
 
 def get_urls(x):
-    return utils._get_urls(x):
+    return utils._get_urls(x)
 
 def remove_urls(x):
-	return utils._remove_urls(x):
+	return utils._remove_urls(x)
 
 def remove_rt(x):
-	return utils._remove_rt(x):
+	return utils._remove_rt(x)
 
 def remove_special_chars(x):
-	return utils._remove_special_chars(x):
+	return utils._remove_special_chars(x)
 
 def remove_html_tags(x):
-    return utils._remove_html_tags(x):
+    return utils._remove_html_tags(x)
 
 def remove_accented_chars(x):
-    return utils._remove_accented_chars(x):
+    return utils._remove_accented_chars(x)
 
 def make_to_base(x):
     return utils._make_to_base(x)
